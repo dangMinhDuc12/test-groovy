1dfe8601a792702fb8e2d5db65417d5ebbfec1a3
Duc Dang <dangminhduca3@gmail.com>
2024-06-06 10:23:54 +0700

    change pipeline

--- vars/myLibraryFunctions.groovy
+++ vars/myLibraryFunctions.groovy
@@ -5,3 +5,18 @@ def functionOne(param) {
 def functionTwo() {
   echo "Hello from function two"
 }
+
+def testGetBuildCacheSize() {
+   // Get Docker build cache size
+                    def buildCacheSize = sh(script: """
+                        docker system df --format '{{json .}}' | \
+                        jq -r '.[] | select(.Type == "Build Cache") | .Size'
+                    """, returnStdout: true).trim()
+                    
+                    // Convert size to bytes for comparison 
+                    def buildCacheSizeBytes = sh(script: """
+                        numfmt --from=iec --to=bytes '$buildCacheSize'
+                    """, returnStdout: true).trim().toInteger()
+
+                    echo ${buildCacheSizeBytes}
+}
