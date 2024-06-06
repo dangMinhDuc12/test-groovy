e8f5aef65abbca7db36b54e043b7f5d76e8b37b8
Duc Dang <dangminhduca3@gmail.com>
2024-06-06 10:40:07 +0700

    change pipeline

--- vars/myLibraryFunctions.groovy
+++ vars/myLibraryFunctions.groovy
@@ -10,5 +10,5 @@ def testGetBuildCacheSize() {
   def dockerDfOutput = sh(script: 'docker system df --format "{{.BuildCache}}" | awk \'{print $1}\'', returnStdout: true).trim()
                     def cacheSizeGB = (dockerDfOutput.toFloat() / 1024 / 1024 / 1024).round(2)  // Convert to GB
 
-                    echo "Current build cache size: ${cacheSizeGB} GB
+                    echo "Current build cache size: ${cacheSizeGB} GB"
 }
